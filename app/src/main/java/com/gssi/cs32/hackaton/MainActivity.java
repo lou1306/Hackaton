f6a4ced83c7ace1cc651ea8296fbf289d7dd68e2
Luca Di Stefano <luca.distefano@gssi.it>
2017-07-07 16:38:48 +0200

    Use my version of the app

--- app/src/main/java/com/gssi/cs32/hackaton/MainActivity.java
+++ app/src/main/java/com/gssi/cs32/hackaton/MainActivity.java
@@ -47,6 +47,8 @@ import java.io.InputStream;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
+import cs32.gssi.hackaton.R;
+
 public class MainActivity extends AppCompatActivity {
     private MapView mMapView;
     private int requestCode = 2;
@@ -167,10 +169,6 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
-
-        // Example of a call to a native method
-    TextView tv = (TextView) findViewById(R.id.sample_text);
-    tv.setText(stringFromJNI());
     }
 
 
@@ -195,17 +193,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
-
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
     }
 
     @Override
